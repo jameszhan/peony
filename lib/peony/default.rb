ffcfc13f557932d916ff33366f7f962e2e25ab23
James Zhan <zhiqiangzhan@gmail.com>
2013-08-19 00:18:11 +0800

    Use lambda replace block.

--- lib/peony/default.rb
+++ lib/peony/default.rb
@@ -1,10 +1,10 @@
 set_default :base_dir, "/u"
-set_default(:var_dir){"#{base_dir}/var"}
-set_default(:etc_dir){"#{base_dir}/etc"}
-set_default(:run_dir){"#{var_dir}/run"}
-set_default(:log_dir){"#{var_dir}/log"}
-set_default(:www_dir){"#{var_dir}/www"}
-set_default(:data_dir){ "#{var_dir}/data"}
+set_default :var_dir, ->{"#{base_dir}/var"}
+set_default :etc_dir, ->{"#{base_dir}/etc"}
+set_default :run_dir, ->{"#{var_dir}/run"}
+set_default :log_dir, ->{"#{var_dir}/log"}
+set_default :www_dir, ->{"#{var_dir}/www"}
+set_default :data_dir, ->{"#{var_dir}/data"}
 
 namespace :settings do
   task :list do
